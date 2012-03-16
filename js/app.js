ba3c7a5b68f89ddb241f013b194823aaf0b1d3ba
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-03-16 11:21:20 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,56 @@
+Todos = Ember.Application.create();
+
+Todos.Todo = Ember.Object.extend({
+  title: null,
+  isDone: false
+});
+ 
+Todos.todosController = Ember.ArrayController.create({
+  // Initialize the array controller with an empty array.
+  content: [],
+ 
+  // Creates a new todo with the passed title, then adds it
+  // to the array.
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({ title: title });
+    this.pushObject(todo);
+  },
+
+  remaining: function() {
+    return this.filterProperty('isDone', false).get('length');
+  }.property('@each.isDone'),
+
+  clearCompletedTodos: function() {
+    this.filterProperty('isDone', true).forEach(this.removeObject, this);
+  },
+
+  allAreDone: function(key, value) {
+    if (value !== undefined) {
+      this.setEach('isDone', value);
+ 
+      return value;
+    } else {
+      return !!this.get('length') && this.everyProperty('isDone', true);
+    }
+  }.property('@each.isDone')
+});
+
+Todos.CreateTodoView = Ember.TextField.extend({
+  insertNewline: function() {
+    var value = this.get('value');
+ 
+    if (value) {
+      Todos.todosController.createTodo(value);
+      this.set('value', '');
+    }
+  }
+});
+
+Todos.StatsView = Ember.View.extend({
+  remainingBinding: 'Todos.todosController.remaining',
+ 
+  remainingString: function() {
+    var remaining = this.get('remaining');
+    return remaining + (remaining === 1 ? " item" : " items");
+  }.property('remaining')
+});
